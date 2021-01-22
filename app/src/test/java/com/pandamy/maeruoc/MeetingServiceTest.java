13ab8edc38162d91030f51a2295b72cbfbd1638e
Maxvyr <pandamydev@gmail.com>
2021-01-22 12:13:29 +0100

    test unitaire ok

--- app/src/test/java/com/pandamy/maeruoc/MeetingServiceTest.java
+++ app/src/test/java/com/pandamy/maeruoc/MeetingServiceTest.java
@@ -3,9 +3,11 @@ package com.pandamy.maeruoc;
 
 import com.pandamy.maeruoc.di.DI;
 import com.pandamy.maeruoc.models.Meeting;
+import com.pandamy.maeruoc.models.Member;
 import com.pandamy.maeruoc.models.Room;
 import com.pandamy.maeruoc.models.RoomColor;
 import com.pandamy.maeruoc.service.ApiService;
+import com.pandamy.maeruoc.service.DummyGenerator;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +16,7 @@ import org.junit.runners.JUnit4;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -41,7 +44,6 @@ public class MeetingServiceTest {
         assertFalse(meetingList.contains(meetingToDelete));
     }
 
-
     /**
      * Add Meeting with success
      */
@@ -62,5 +64,45 @@ public class MeetingServiceTest {
         Meeting meetingCreated = service.createMeeting(100,"Created","00:23",new Room(5, "Bowser",RoomColor.COLOR_BROWN),"kaori@lamzone.com");
         service.addMeeting(meetingCreated);
         assertTrue(meetingList.contains(meetingCreated));
+    }
+
+    /**
+     * Get Meetings
+     */
+    @Test
+    public void getMeetingsWithSuccess() {
+        List<Meeting> meetingListBase = DummyGenerator.generatorOfDummyMeetings();
+        List<Meeting> meetingListRecover = service.getMeetings();
+        assertEquals(meetingListBase.size(), meetingListRecover.size());
+    }
+
+    /**
+     * Get Rooms
+     */
+    @Test
+    public void getRoomsWithSuccess() {
+        List<Room> roomsListBase = DummyGenerator.generatorOfDummyRooms();
+        List<Room> roomsListRecover = service.getRooms();
+        assertEquals(roomsListBase.get(0), roomsListRecover.get(0));
+    }
+
+    /**
+     * Get RoomsName
+     */
+    @Test
+    public void getRoomsNameWithSuccess() {
+        List<String> roomsNameListBase = DummyGenerator.getRoomsNameOfDummyRooms();
+        List<String> roomsNameListRecover = service.getRoomsName();
+        assertEquals(roomsNameListBase.get(1), roomsNameListRecover.get(1));
+    }
+
+    /**
+     * Get Members
+     */
+    @Test
+    public void getMembersWithSuccess() {
+        List<Member> membersListBase = DummyGenerator.generatorOfDummyMembers();
+        List<Member> membersListRecover = service.getMembers();
+        assertEquals(membersListBase.get(0), membersListRecover.get(0));
     }
 }
